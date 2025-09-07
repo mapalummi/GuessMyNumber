4e7a8be7dfcf84c2e1c904aa33c51f42711b1115
Marco <marco.palummieri@gmail.com>
2025-09-07 23:57:09 +0200

    Refactor game logic to initialize high score tracking and clean up code formatting

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = (document.querySelector('.score').textContent = 20);
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
-
-
 
   // CHALLENGE EXERCISE
   document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-
     document.querySelector('.number').textContent = '?';
     document.querySelector('.score').textContent = score;
     document.querySelector('.guess').value = '';
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
   });
-
-
-
 
   //   When there is no input
   if (!guess) {
@@ -37,6 +31,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // highScore = score;
+    // document.querySelector('.highscore').textContent = highScore;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
